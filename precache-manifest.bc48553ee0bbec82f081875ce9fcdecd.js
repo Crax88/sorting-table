3ac5c40e71d4a3a1399adb128cd4708bdb09678f
Nikita <ntrafilkin88@gmail.com>
2019-12-25 14:53:03 +0300

    Updates

---
+++ precache-manifest.bc48553ee0bbec82f081875ce9fcdecd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a02afccf2433ab48793d2fcfd1257aa",
+    "url": "/sorting-table/index.html"
+  },
+  {
+    "revision": "3abf7fe4e919fa55111f",
+    "url": "/sorting-table/static/css/main.0204f6f5.chunk.css"
+  },
+  {
+    "revision": "741214e4638d27ee26e6",
+    "url": "/sorting-table/static/js/2.490b3271.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/sorting-table/static/js/2.490b3271.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3abf7fe4e919fa55111f",
+    "url": "/sorting-table/static/js/main.becf7542.chunk.js"
+  },
+  {
+    "revision": "dfd5ec832e9410ba48d8",
+    "url": "/sorting-table/static/js/runtime-main.e62c517c.js"
+  },
+  {
+    "revision": "5656c814cfc5ebe857daf380038e9676",
+    "url": "/sorting-table/static/media/downArrow.5656c814.svg"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/sorting-table/static/media/leftArrow.6c9efe0c.svg"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/sorting-table/static/media/preloader.9064981b.svg"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/sorting-table/static/media/rightArrow.be237328.svg"
+  }
+]);
